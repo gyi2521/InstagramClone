5c3813d698838a6cf4c5bbef56b0a2f398b55c50
gyi <ginay2521@gmail.com>
2020-09-01 16:59:27 -0400

    changed the connection string to monogodbatlas

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://user:password3@ds023478.mlab.com:23478/heroku_wb8x471d', { useNewUrlParser: true });
+//Migrating from mlab to MongoDBAtlas---mongoose.connect('mongodb://user:password3@ds023478.mlab.com:23478/heroku_wb8x471d', { useNewUrlParser: true });
 //mongoose.connect('mongodb://localhost/instagram',{ useNewUrlParser: true });
+mongoose.connect('mongodb+srv://gyi:NamJee01@clusterinstagram.odqm0.mongodb.net/instagramClone?retryWrites=true&w=majority');
 
 require('./routes/api-routes')(app);
 require('./routes/html-routes')(app);
